a4ff1b06a8848cb99228d05783e819c0c8c71147
Andressa Rabêlo <dre.emy89@gmail.com>
2024-03-25 15:12:21 -0300

    25/03/2024

---
+++ Programação de Soluções Computacionais/EstruturaDeSelecao/EstruturaDeRepeticao_For.java
@@ -0,0 +1,12 @@
+public class EstruturaDeRepeticao_For {
+    public static void main (String[] args) {
+        int numero = 6;
+        for(int i = 0; i < numero; i++) {
+            //int i começa valendo 0, enquanto i for menor que 10, aumente 1;
+            System.out.println(i);
+        }
+    }
+   
+}
+    
+
