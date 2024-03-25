d24a562844911120dd07d7684edd1e6333223451
Andressa Rabêlo <dre.emy89@gmail.com>
2024-03-25 15:13:56 -0300

    25/03/2024

---
+++ Programação de Soluções Computacionais/EstruturaDeSelecao/EstruturaDeRepeticao_While.java
@@ -0,0 +1,18 @@
+ public class EstruturaDeRepeticao_While {
+        public static void main(String[] args) {
+            int numero = 0;
+            while (numero <=5) {
+                //ENQUANTO
+                //ter cuidado para usar, pq ewle soma depois que imprime
+                System.out.println(numero);
+                numero+=2;
+                //acrescentar 2
+                //sem isso, ele vai seguir no valor 0 de forma infinita;
+            }
+           
+           
+        }
+       
+    }
+    
+
