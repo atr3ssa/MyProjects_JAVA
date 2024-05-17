2594faf46241d1908a7fe09ff2e0a5d02df45622
Andressa Rabêlo <dre.emy89@gmail.com>
2024-05-17 17:19:19 -0300

    Curso java

---
+++ Java/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+//tudo que estiver com ".java" em Projects, é uma classe
+package primeiroprograma;
+//pacote sempre em mínuscula
+
+public class PrimeiroPrograma {
+    //Classe sempre com as iniciais em maíuscula
+
+    public static void main(String[] args) {
+        //System.out.print("Ola, mundo!");
+        System.out.println("Olá, Mundo!");
+                
+    }
+    
+}
