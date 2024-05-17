b8b3b78698a47847870105975ac9c275f3aca5b2
Andressa Rabêlo <dre.emy89@gmail.com>
2024-05-17 15:50:59 -0300

    Curso Java

---
+++ Java/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+//tudo que estiver com ".java" em Projects, é uma classe
+package primeiroprograma;
+//pacote sempre em mínuscula ex: alunosjava
+
+public class PrimeiroPrograma {
+    //Classe sempre com as iniciais em maíuscula e o restante em CamelCase: AlunosJava
+
+    public static void main(String[] args) {
+        //método sempre com a primeira letra em minusculo e o restante em CamelCase, ex: main, alunosJava
+        //psvm + TAB = atalho ^
+        //System.out.println("Ola, mundo!");
+        System.out.println("Olá, Mundo!");
+        //sout + TAB = atalho ^ 
+        //System é uma classe, por isso a inicial é maíuscula
+        //.print é um método, pois a primeira é minusc, enquanto o restante é camelcase
+                
+    }
+    
+}
