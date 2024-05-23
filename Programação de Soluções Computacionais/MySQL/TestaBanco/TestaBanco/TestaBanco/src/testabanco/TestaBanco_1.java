2341c1c41b4000fcbf004018f5dd5b87ffdc45a2
Andressa Rabêlo <dre.emy89@gmail.com>
2024-05-22 23:01:03 -0300

    cc

---
+++ Programação de Soluções Computacionais/MySQL/TestaBanco/TestaBanco/TestaBanco/src/testabanco/TestaBanco_1.java
@@ -0,0 +1,13 @@
+
+package testabanco;
+
+
+public class TestaBanco_1 {
+
+  
+    public static void main(String[] args) {
+        ConexaoDB con = new ConexaoDB();
+        con.obtemConexao();
+    }
+    
+}
