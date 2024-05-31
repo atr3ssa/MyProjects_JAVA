19e279fd31c18764d060be29c7de02cebea6a648
Andressa Rabêlo <dre.emy89@gmail.com>
2024-05-31 13:16:50 -0300

    cc

--- Programação de Soluções Computacionais/MySQL/TestaBanco/TestaBanco/TestaBanco/src/testabanco/Usuario.java
+++ Programação de Soluções Computacionais/MySQL/TestaBanco/TestaBanco/TestaBanco/src/testabanco/Usuario.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package testabanco;
 
-/**
- *
- * @author DENISE NEVES
- */
+
 public class Usuario {
 
       private int codigo;
