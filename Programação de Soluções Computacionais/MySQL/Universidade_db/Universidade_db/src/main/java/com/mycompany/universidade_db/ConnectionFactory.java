f6bf560b62858ccd8738d3f2c610fbb97e7a6780
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-01 12:40:01 -0300

    cc

--- Programação de Soluções Computacionais/MySQL/Universidade_db/Universidade_db/src/main/java/com/mycompany/universidade_db/ConnectionFactory.java
+++ Programação de Soluções Computacionais/MySQL/Universidade_db/Universidade_db/src/main/java/com/mycompany/universidade_db/ConnectionFactory.java
@@ -1,10 +1,9 @@
 
-package com.mycompany.bancodedados;
+package com.mycompany.universidade_db;
 
-//Add importações SQL
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.SQLException;
 
 public class ConnectionFactory {
     // usuario do MySQL que uso p entrar
@@ -15,7 +14,8 @@ public class ConnectionFactory {
     //porta na qual o servidor vai ficar recebendo as requisições, é padrão p/ o MySQL 
     private String porta = "3306";
     // nome do banco de dados
-    private String bd = "psc";
+    private String bd = "Universidade";
+    private String timezone = "America/Sao_Paulo"; //add o fuso horário
     
     //Criar método do tipo do objeto, mesmo tipo que o da importação "import java.sql.Connection;"
     // posso criar um método connection do tipo da classe Connection
@@ -25,14 +25,30 @@ public class ConnectionFactory {
             //usado para mapear o erro do seu código
             //importante, pois por ser uma conexão cliente e servidor, o que pode facilmente falhar
         try{
-            Connection conectar = DriverManager.getConnection("jdbc:mysql://"+host+":"+porta+"/"+ bd,
-                    usuario,
-                    senha
-            );
+            String url = "jdbc:mysql://"+host+":"+porta+"/"+ bd +"?serverTimezone=" + timezone;
+            
+            Connection conectar = DriverManager.getConnection(url,usuario, senha);
+            
+            
+            if (conectar !=null){
+                System.out.println("Conexão estabelecida com sucesso!");
+            }
             return conectar;
-        }catch(Exception e){
+        }catch(SQLException e){
+            System.out.println("Erro ao tentar estabelecer conexão com o banco de dados:");
             e.printStackTrace();
             return null;
         }
     
-}}
+}
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.obtemConexao();
+        
+        if(connection != null){
+            System.out.println("Conexão testada e aprovada!");
+        } else {
+            System.out.println("Falha na conexão!");
+        }
+    }
+}
