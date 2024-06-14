c7c0d5a53812cfb52d2dc5a8e59d5ebea0e616ab
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-14 15:12:46 -0300

    ada

--- Java/Boolean_aula/src/boolean_aula/Vetores.java
+++ Java/Boolean_aula/src/boolean_aula/Vetores.java
@@ -4,10 +4,36 @@
  */
 package boolean_aula;
 
-/**
- *
- * @author DreEm
- */
+import java.util.Arrays;
+
+
 public class Vetores {
-    
+    public static void main(String[] args) {
+        //Não misturar tipos no Array = Vetor
+        
+        //array com 5 posições
+        //1° forma:
+        //String[] letras = new String[5];
+        //p referenciar é usado indice, o 1° é sempre 0 no java
+        //[0] [1] [2] [3] [4]
+        //letras[0]= "a";
+        //letras[1]= "b";
+        //letras[2]= "c";
+        //letras[3]= "y";
+        //letras[4]= "z";
+        
+        //para mostrar o que tem dentro de um array, preciso por o sout dentro de um for (laço de repetição)
+        //for(int i=0; i< letras.length; i++){
+          // System.out.println(letras[i]); 
+        //}
+        
+        //2° forma:
+        //String[] letras = {"a", "b", "c", "y", "z"};
+        //System.out.println(Arrays.toString(letras));
+        
+        //3° forma
+        
+        
+        
+    }
 }
