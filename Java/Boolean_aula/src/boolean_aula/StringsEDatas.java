5dffb0ffc31b9006c301532a0c08dd6405d0e681
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-14 13:39:56 -0300

    ada

---
+++ Java/Boolean_aula/src/boolean_aula/StringsEDatas.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boolean_aula;
+//importações
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class StringsEDatas {
+    public static void main(String[] args) {
+        //Olá + nome de alguem. Hoje é + diadasemana, bom dia
+        
+        //declarar variaveis
+        String nome= "Andressa";
+        
+        //Manipulação de Strings e Datas
+        
+        //Nome em maiúsculas
+        System.out.println(nome.toUpperCase());
+        //Nome em minúsculo
+        System.out.println(nome.toLowerCase());
+        //Consultar quantos caracteres tem a String (comprimento)
+        System.out.println(nome.length());
+        
+        String nomeOutro= "andressa";
+        
+        //Comparar Strings (se variar o o tamanho da letra, da como false, do contrario é true
+        System.out.println(nome.equals(nomeOutro));
+        //Compara Strings, mas ignora o tamanho das letras
+        System.out.println(nome.equalsIgnoreCase(nomeOutro));
+        
+        //ISO 0601 = Define o padão das datas
+        //data com as informações locais
+        //ano mês dia
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+        
+        //Locale nome da variavel= brasl = novo objeto locale("idioma", "país");
+        Locale brasil = new Locale("pt","BR" );
+        
+        //mostrar hoje.diadasemana em português (getDisplayName(estilo, variavel Locale)
+        System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+        
+        //mostrar hoje.diadomes
+        System.out.println(hoje.getDayOfMonth());
+        
+        //mostrar hoje.diadoano
+        System.out.println(hoje.getDayOfYear());
+           
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.new();
+        if(agora.getHour() >= 8 && agora.getHour() <12){
+            
+        }
+        
+    }
+  
+}
