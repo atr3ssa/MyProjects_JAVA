31e533e52f6e2f545e95ca2fb920d8a1f24b7ada
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-17 14:16:49 -0300

    ada

--- Java/Boolean_aula/src/boolean_aula/Funcoes.java
+++ Java/Boolean_aula/src/boolean_aula/Funcoes.java
@@ -4,20 +4,34 @@ package boolean_aula;
 
 public class Funcoes {
     //método = função de partida
-    public static void main(String[] args) {
+    public static void main(String[] args) {//escopo
         //farei o mesmo, mas dentro de uma função
         //String nome = "Andressa";
         //System.out.println("Hello " + nome);
         
+        //Função sem retorno
         String nomeOriginal = "let's Code!";
         //chamar a função/método AQUI, dentro do main. Com o paramentro dentro dos ()
         
         //escopo de função
         saudacao(nomeOriginal);
+        
+        //Função com retorno
+        int resultado = soma(2,3);
+        System.out.println(resultado);
     }
+    //As variaveis de escopos diferentes não tem nenhuma relação, desse modo  o nome tem obrigação de ser igual, apenas dentro do mesmo escopo
     //funcao/método Saudacao
     //saudacao(passagem de parametros)
-    public static void saudacao(String nomeParametro) {
-        System.out.println("Hello" + nomeParametro);
+    //Sem retorno
+    public static void saudacao(String nomeParametro) {//escopo
+        System.out.println("Hello " + nomeParametro);
+    }
+    
+    //função com retorno
+    public static int soma(int a, int b) {
+        //p retornar a soma p a variavel resultado lá no main
+        return a+b;
+        
     }
 }
