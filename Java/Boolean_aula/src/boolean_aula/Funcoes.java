ac9e7ecc18e42a2f358ce315d30932118c47214e
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-17 14:03:28 -0300

    ada

---
+++ Java/Boolean_aula/src/boolean_aula/Funcoes.java
@@ -0,0 +1,20 @@
+
+package boolean_aula;
+
+
+public class Funcoes {
+    //método de partida
+    public static void main(String[] args) {
+        //farei o mesmo, mas dentro de uma função
+        //String nome = "Andressa";
+        //System.out.println("Hello " + nome);
+        
+        //chamar a função AQUI, dentro do main
+        
+        saudacao();
+    }
+    //funcao Saudacao
+    public static void saudacao() {
+        System.out.println("Hello Andressa");
+    }
+}
