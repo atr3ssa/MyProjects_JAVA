7fc438e3e65a748c09656d3e8b58a80d81da4c9b
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-17 16:40:00 -0300

    ada

---
+++ Ada tech/CursoPOO/src/cursopoo/CursoPOO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cursopoo;
+
+
+public class CursoPOO {
+
+   //método/função main
+   //5 caracteristicas:
+    // publico, estático, void (não retorne valor nenhum), o nome deve ser main, vai receber como parametro um array(String[]) ou var args (String...(indefini quantas varáveis eu vou estar recebendo nesse método)) de Strings;
+    public static void main(String[] args) {
+        //atalho sout
+        System.out.println("hello, World!");
+    }
+    
+}
