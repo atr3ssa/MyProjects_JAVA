bbd3f049a0f73078aa6257f7aa8683900c4db024
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-18 21:10:21 -0300

    ada

--- Ada tech/CursoPOO/src/animais/Cachorro.java
+++ Ada tech/CursoPOO/src/animais/Cachorro.java
@@ -15,23 +15,23 @@ public class Cachorro {
     //Métodos = interagir com objeto/ações (objeto executa a ação)
     
     //Métodos:
-    
+    //Método que não faz nada
     public void comer (){
         //assinatura
         //método público, não tem retorno, se chama "comer", não recebe variável nenhuma e não faz nada
         
     }
-    
+    //Método que só imprime algo
     public void latir (){
         System.out.println("Au au");
     }
-    
+    //Método que retorna algo
     public String pegar(){
         //agr o método terá retorno
         return "bolinha";
         //tem que por "retorno" e algo com o mesmo tipo do método;   
     }
-    
+    //método que recebe e retorna
     public String interagir (String acao){
         //validação
        if(acao.equals("carinho")){
