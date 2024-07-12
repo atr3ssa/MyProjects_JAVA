66d97eb7b995618d53d886370e77c1875acf98be
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-12 12:18:06 -0300

    ada

--- Ada tech/SuperMarket/src/main/Main.java
+++ Ada tech/SuperMarket/src/main/Main.java
@@ -1,6 +1,36 @@
 //pacote
 package main;
 
+//importações
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {//classe executável
+        Scanner scanner = new Scanner (System.in); 
+        int opcao;
+        do{
+            System.out.println("\n Lista de Compras");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Sair");
+            System.out.println("Escolha uma opção:");
+            opcao = scanner.nextInt();
+            
+            switch(opcao){
+                case 1:
+                    System.out.println("Digite o item a ser inserido:");
+                    String item = scanner.next();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+            
+        }
+        
+    }
 }
