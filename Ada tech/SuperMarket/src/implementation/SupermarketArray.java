4a17b575cd9487e74bba33d6ced0de8dadbe398c
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-12 11:54:10 -0300

    ada

--- Ada tech/SuperMarket/src/implementation/SupermarketArray.java
+++ Ada tech/SuperMarket/src/implementation/SupermarketArray.java
@@ -5,7 +5,7 @@ public class SupermarketArray implements Supermarket{
     //dados utilizados pela classe:
     private final String [] itens; // vetor
     
-    private int LastIndex;
+    private int LastIndex; // ponteiro
     
     //inicializar vetor com o construtor:
     public SupermarketArray(int Size) {
@@ -13,18 +13,20 @@ public class SupermarketArray implements Supermarket{
         LastIndex = -1; //indicar o último elemento inserido // -1 pq não há último elemento  
     }
 
-    
-    
-
     //assinatura dos métodos:
     @Override
     public void add(String item) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(LastIndex == itens.length-1){ // validação (se o ponteiro(LastIndex) for igual ao último espaço da lista (itens.lengyh-1), mostre que a lista está cheia
+            System.err.println("Lista de Supermercado cheia"); // utilizei o err, ao invés do out, só para aparecer em vermelho
+        } else { // do contrário
+            LastIndex ++; // add mais um
+            itens[LastIndex] = item; //guardar no objeto item
+        }
     }
 
     @Override
     public void print() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
     }
 
     @Override
