7650b64d5d4d0f6b839b6b36586b0cc05ebd417b
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-24 12:03:19 -0300

    cc

--- Programação de Soluções Computacionais/Listaexercicios/exercicios.java/ex9.java
+++
@@ -1,8 +0,0 @@
-// Faça um programa para exibir 50 vezes o seu nome na tela
-public class Main {
-public static void main(String[] args) {
-
-for (int i = 0; i < 50; i++) {
-      System.out.println("Nome");
-    }
-}}
